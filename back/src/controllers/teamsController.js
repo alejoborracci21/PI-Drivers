e43244cb4f25a26c50d0f7ff949504d431b1655c
alejoborracci21 <borraccialejo@hotmail.es>
2024-02-14 09:11:49 -0300

    back end

---
+++ back/src/controllers/teamsController.js
@@ -0,0 +1,30 @@
+// teamsController.js
+
+const { Team } = require('../db');
+
+const addTeamsToDatabase = async (teams) => {
+  try {
+    const uniqueTeams = [...new Set(teams)];
+
+    // Filtrar equipos que ya existen en la base de datos
+    const teamsToAdd = await Promise.all(
+      uniqueTeams.map(async (teamName) => {
+        const existingTeam = await Team.findOne({ where: { name: teamName } });
+        return !existingTeam ? { name: teamName } : null;
+      })
+    );
+
+    // Filtrar equipos nulos (que ya existían)
+    const filteredTeamsToAdd = teamsToAdd.filter((team) => team !== null);
+
+    // Agregar equipos a la base de datos
+    await Team.bulkCreate(filteredTeamsToAdd);
+
+    return filteredTeamsToAdd;
+  } catch (error) {
+    console.error('Error al agregar equipos a la base de datos:', error);
+    throw error;
+  }
+};
+
+module.exports = { addTeamsToDatabase };
--- back/src/routes/teams.js
+++ back/src/routes/teams.js
@@ -1,15 +1,29 @@
-const {Router} = require('express')
-const  axios  = require('axios')
-const URL = ''
+// Tu ruta principal
 
-const route = Router()
+const { Router } = require('express');
+const axios = require('axios');
+const { addTeamsToDatabase } = require('../controllers/teamsController');
+const api = require('../../api/db.json');
+const { drivers } = api;
+const route = Router();
 
-route.get('/', async(req, res) => {
-    try {
-        res.send('get a ruta teams/')
-    } catch (error) {
-        res.send(error)
-    }
-})
+route.get('/', async (req, res) => {
+  try {
+    const allTeams = [];
 
-module.exports = route
+    drivers.forEach((driver) => {
+      const driverTeams = driver.teams ? driver.teams.split(",") : [];
+      allTeams.push(...driverTeams.map((team) => team.trim()));
+    });
+
+    //? Funcion para agregar los teams a la db
+    // const teamsAdded = await addTeamsToDatabase(allTeams); 
+
+    res.send(allTeams);
+  } catch (error) {
+    console.error('Error en la ruta principal:', error);
+    res.status(500).send({ error: 'Error interno del servidor', details: error.message });
+  }
+});
+
+module.exports = route;
