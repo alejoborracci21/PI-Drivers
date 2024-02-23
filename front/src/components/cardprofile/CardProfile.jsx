b42dcf8fc8bea4ef62428870e5743252aea47a05
alejoborracci21 <borraccialejo@hotmail.es>
2024-02-22 22:13:00 -0300

    add aboutpage

---
+++ front/src/components/cardprofile/CardProfile.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+import { Redirect } from 'react-router-dom';
+
+
+const CardProfile = () => {
+    return (
+        <div className="div-header">
+            <image></image>
+            <h3>Alejo Borracci</h3>
+            <h5>Ciudad: Rosario, Santa Fe</h5>
+            <h5>Telefono: 3465-656664</h5>
+            <div className="div-redes">
+            <h5>Mis Redes:</h5>
+
+            </div>
+            
+        </div>
+    )
+}
--- front/src/pages/aboutpage/aboutme.jsx
+++ front/src/pages/aboutpage/aboutme.jsx
@@ -1,11 +1,16 @@
 import React from "react";
-import NavBar from "../../components/navbar/navbar";
+import NavBar from "../../components/navbar/navBar";
 
 const AboutMe = () => {
     return(
         <div>
             <NavBar></NavBar>
-            <h1>soy el about</h1>
+            
+            <h1>Mi proyecto individual Drivers</h1>
+            <p>Este es mi proyecto individual en SoyHenry, pertenezco a la cohorte FS-46B.
+                En este proyecto utilice las tecnologias React, Redux, Vite, Sequelize, Postgres
+                y los lenguajes de programacion Javascript, CSS y HTML5.
+            </p>
         </div>
     )
 }
